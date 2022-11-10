5956fddc30f1a50ececb2233c25eda6069b8874b
brunocereza <bruno.cereza@hotmail.com>
2022-11-10 00:18:04 -0300

    alterando MQTT

--- src/index.ts
+++ src/index.ts
@@ -24,17 +24,45 @@ route.get("/verifyRfid", async (req: Request, res: Response): Promise<void> => {
   const rfidMqtt = req.query.rfid ?? "";
   if (rfidMqtt) {
     const retorno = await rfid.verifyRfid(String(rfidMqtt));
-
     if (retorno) {
-      res.status(200).json(retorno);
-      return;
+      res.status(200).send(retorno);
+    } else {
+      res.status(404).send("RFID não encontrado");
     }
-    res.status(404).json("RFID não encontrado");
-    return;
   } else {
-    res.status(422).json("RFID É obrigatório");
-    return;
+    res.status(412).send("RFID OBRIGATORIO");
   }
+});
+
+route.get("/mockData", async (req: Request, res: Response): Promise<any> => {
+  const mock = [
+    {
+      id: Math.random().toString(),
+      data: "2022-11-09 23:19",
+      quantidade: 11.15,
+    },
+    {
+      id: Math.random().toString(),
+      data: "2022-11-08 08:29",
+      quantidade: 130,
+    },
+    {
+      id: Math.random().toString(),
+      data: "2022-10-07 07:34",
+      quantidade: 30,
+    },
+    {
+      id: Math.random().toString(),
+      data: "2022-05-03 00:45",
+      quantidade: 100,
+    },
+    {
+      id: Math.random().toString(),
+      data: "2022-11-10 03:19",
+      quantidade: 35.12,
+    },
+  ];
+  res.status(200).json(mock);
 });
 
 app.use(route);
